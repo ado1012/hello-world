fdf5306cc70175cf9d9794ca314bf1af59fa453c
ado1012 <43334324+ado1012@users.noreply.github.com>
2018-09-28 22:04:14 +0700

    Update 2_Modul42.py

--- 2_Modul42.py
+++ 2_Modul42.py
@@ -1,9 +1,10 @@
 num = []
-def modulus():
-    for i in range(1, 11):
-        num.append(int(input("input number (u'll be askd 10 times")))
-    for i in num:
-        x = int(i) % 3
-        print(x)
+x = []
 
-modulus()
+for i in range(1, 11):
+    num.append(int(input("input number (u'll be askd 10 times) ")))
+for i in num:
+    x.append(int(i) % 42)
+z = (dict((y,x.count(y)) for y in set(x)))
+
+print(len(z.keys()))
